1a2f8e2db07263bef02b648c8985c51dfcc44161
刘景友 <liujy@oriental-finance.com>
2017-06-23 17:20:56 +0800

    活动报名

---
+++ app/src/main/java/com/sports/limitsport/activity/SignUpActivity.java
@@ -0,0 +1,11 @@
+package com.sports.limitsport.activity;
+
+import com.sports.limitsport.base.BaseActivity;
+
+/**
+ * Created by liuworkmac on 17/6/23.
+ * 活动报名
+ */
+
+public class SignUpActivity extends BaseActivity{
+}
